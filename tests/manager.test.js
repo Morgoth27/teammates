2573ba05d650f4bab45e03a9ae24a5fcb37847ee
Tiffany <leopardmitai@gmail.com>
2022-01-27 02:37:12 -0800

    Finished test for manager, it was successul

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,1 +1,28 @@
-const manager = require('../js/manager.js');
+const Manager = require('../lib/manager.js');
+
+describe("Manager", () => {
+    const employee = {
+        name: "Tiffany",
+        id: 45,
+        email: "email@email.com",
+        officeNum: "12"
+    };
+
+    describe("getOfficeNumber", () =>{
+        it("gathering office number that was inputted", () => {
+            const newPerson = new Manager(employee.name, employee.id, employee.email, employee.officeNum);
+            const result = newPerson.getOfficeNumber();
+            console.log(result);
+
+            expect(result).toBe(employee.officeNum)
+        });
+    });
+
+    describe("getRole", () => {
+        it("should get intern role", () => {
+            const newPerson = new Manager(employee.name, employee.id, employee.email, employee.officeNum);
+            expect(newPerson.getRole()).toEqual('Manager');
+
+        });
+    });
+});
